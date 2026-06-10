ab2d5b8f9c52708679aed91d97284719d9c405bb
23209zl <23209zl@hvhs.school.nz>
2026-06-10 02:53:29 +0000

    I made a change

---
+++ BballGame/BballGame.js
@@ -0,0 +1,250 @@
+/*******************************************************/
+// Game Project
+/// Written by Jackson
+/*******************************************************/
+
+/*******************************************************/
+// setup()
+/*******************************************************/
+
+//Variables
+const height = 900;
+const width = 900;
+let totalTime = 60;
+let startTime;
+let timeRemaining = totalTime;
+let runningTime;
+let gameStarted = false;
+let gameEnded = false;
+let score = 0;
+let speed = 3;
+let lives = 3;
+
+function preload() {
+    imgBG = loadImage('background.jpg');
+    imgBball = loadImage('bball.png');
+    imgBackboard = loadImage('BballBackboard.png');
+}
+//images for the basketball, backboard and game screen
+
+function setup() {
+    cnv = new Canvas(width, height);
+
+    wallLH = new Sprite(0, height / 2, 20, height, 'k');
+    wallLH.color = 'blue';
+
+    wallRH = new Sprite(width, height / 2, 20, height, 'k');
+    wallRH.color = 'red';
+
+    wallTop = new Sprite(width / 2, 0, width, 20, 'k');
+    wallTop.color = 'blue';
+
+    wallBot = new Sprite(width / 2, height, width, 20, 'k');
+    wallBot.color = 'red';
+    //Walls and boundaries
+
+
+    Bball = new Sprite(width / 2, 650, 100);
+    Bball.image = (imgBball);
+    imgBball.resize(80, 80);
+    //Basketball sprite and image
+
+    BballBackboard = new Sprite(width / 2, 300, 250, 150, 'k');
+    BballBackboard.image = (imgBackboard);
+    imgBackboard.resize(300, 250);
+    //Backboard sprite and image
+
+    StartBtn = new Sprite(width / 2, 800, 150, 50, 'k');
+    StartBtn.color = '#b5fd84';
+    StartBtn.text = 'Start Game';
+    StartBtn.textSize = (25);
+    StartBtn.visible = true;
+    //start button sprite
+
+    HitBox = new Sprite(width / 2, 300, 150, 150, 'k');
+    HitBox.visible = false;
+    //Small hit box instead of using whole backboard as the hit box
+
+    RestartBtn = new Sprite(width / 2, 800, 150, 50, 'k');
+    RestartBtn.color = '#b5fd84';
+    RestartBtn.text = 'Play Again';
+    RestartBtn.textSize = 25;
+    RestartBtn.visible = false;
+}
+
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+    background(imgBG);
+
+    if (StartBtn.mouse.pressed()) {
+        gameStarted = true;
+        StartBtn.visible = false;
+        BballBackboard.vel.x = speed;
+        Bball.rotationSpeed = 3;
+        startTime = millis();
+    }
+    //When start button is pressed game starts (gameStarted = true)
+
+    if (gameStarted) {
+        if (BballBackboard.x <= wallLH.x + 125) {
+            BballBackboard.vel.x = speed;
+        }
+        //When backboard hits left wall, change direction and move right
+        //125 pixies instead of using center point of backboard to change direction so it doesn't go ourside the boundary
+
+        if (BballBackboard.x >= wallRH.x - 125) {
+            BballBackboard.vel.x = -speed;
+        }
+        //When backboard hits right wall, change direction and move left
+        //125 pixies instead of using center point of backboard to change direction so it doesn't go ourside the boundary
+
+        if (kb.presses('space')) {
+            Bball.vel.y = -8;
+        }
+        //Shoots the basketball when space is pressed
+
+        if (Bball.collides(wallTop)) {
+            Bball.vel.x = 0;
+            Bball.vel.y = 0;
+            Bball.x = width / 2;
+            Bball.y = 650;
+            Bball.rotationSpeed = 0;
+            lives = lives - 1;
+        }
+        //When basketball hits the top wall live = live -1. resets the basketball position
+
+
+        if (lives == 0) {
+            BballBackboard.vel.x = 0;
+            Bball.visible = false;
+            BballBackboard.visible = false;
+            gameStarted = false;
+            gameEnded = true;
+        }
+        //If live == 0 gameEnded = true, backboard and basketball visible = false
+
+        if (Bball.collides(BballBackboard)) {
+            Bball.vel.y = 0;
+            Bball.x = width / 2;
+            Bball.y = 650;
+            Bball.vel.x = 0;
+            Bball.rotationSpeed = 3;
+        }
+        //If basketball hits the backboard instead of hitbox scores stays the same, resets the basketball x and y position
+
+
+        if (Bball.collides(HitBox)) {
+            if (timeRemaining <= 30) {
+                score += 2;
+            } else {
+                score += 1;
+            }
+            //Double points at 30 seconds
+
+            Bball.vel.y = 0;
+            Bball.x = width / 2;
+            Bball.y = 650;
+            Bball.vel.x = 0;
+            Bball.rotationSpeed = 3;
+        }
+        //Resets the basketball when hits the backboard and adds the score
+
+        textSize(25);
+        fill("white");
+        text("Score: " + score, 20, 50);
+        //Scores message
+
+        textSize(20);
+        fill("white");
+        text("Press space to shoot the basketball", 300, 750);
+        //How to play (Instructions)
+
+        let currentTime = millis();
+        runningTime = ((currentTime - startTime) / 1000);
+        timeRemaining = totalTime - runningTime;
+
+        if (timeRemaining <= 0) {
+            timeRemaining = 0;
+            gameStarted = false;
+            gameEnded = true;
+            Bball.vel.x = 0;
+            Bball.vel.y = 0;
+            Bball.rotationSpeed = 0;
+            BballBackboard.vel.x = 0;
+            Bball.visible = false;
+            BballBackboard.visible = false;
+        }
+        //if game time = 0 game ends (Bball/Backboard stops moving and visible = false)
+
+        textSize(25);
+        fill("white");
+        text("Time: " + Math.ceil(timeRemaining), 700, 50);
+        //Timer
+
+        if (timeRemaining <= 30) {
+            speed = 6;
+            text("DOUBLE POINTS! LEVEL INCREASED!", 230, height / 2);
+        }
+        //30 seconds level increases (faster backboard speed)
+
+
+        textSize(25);
+        fill("white");
+        text("Lives:", width / 2, 50);
+
+        for (let i = 0; i < lives; i++) {
+            circle(550 + i * 30, 40, 20);
+        }
+        //lives using for loop
+    }
+
+    HitBox.x = BballBackboard.x;
+    HitBox.y = BballBackboard.y;
+    //Hit box follows the backboard, same x and y position/moves with the backboard
+
+    /*******************************************************/
+    // endScreen()
+    /*******************************************************/
+
+    if (gameEnded) {
+        textSize(35);
+        fill('white');
+        text("GAME ENDED! Your Final Score Is " + score + "!", 150, height / 2);
+        //Final score text
+
+        RestartBtn.visible = true;
+
+        if (gameEnded && RestartBtn.mouse.pressed()) {
+            gameEnded = false;
+            gameStarted = true;
+
+            RestartBtn.visible = false;
+
+            BballBackboard.visible = true;
+            Bball.visible = true;
+
+            speed = 3;
+            score = 0;
+
+            startTime = millis();
+            timeRemaining = totalTime;
+
+            BballBackboard.vel.x = speed;
+
+            Bball.rotationSpeed = 3;
+            Bball.vel.y = 0;
+
+            Bball.x = width / 2;
+            Bball.y = 650;
+
+            lives = 3;
+        }
+        //Play again button/restart button
+    }
+}
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
