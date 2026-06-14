693b17ae369a179b63bc6d2f6a3bb629c946889a
23209zl <23209zl@hvhs.school.nz>
2026-06-14 23:00:34 +0000

    I made a change

--- fbjs/fb_script.js
+++ fbjs/fb_script.js
@@ -1,1 +1,20 @@
- firebase.database().ref('game1/users/highScores').set()
+var userAge = document.getElementById("age").value;
+var userName = document.getElementById("name").value;
+var userScore = document.getElementById("score").value;
+var uid;
+
+ if (!userName || !userAge) {
+        alert("You must fill out all fields before submitting the form!");
+        return;
+    }
+
+if (uid == null) {
+        alert("Please Log In First!");
+        return;
+    }
+
+ firebase.database().ref("/game1/" + GLOBAL_user["uid"]).set({
+    userName: GLOBAL_user["userName"],
+    userAge: GLOBAL_user["userAge"],
+    userScore: GLOBAL_user["userScore"]
+  });
