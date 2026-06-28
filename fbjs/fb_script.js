63cca53ede7cefb2c810e400ea0912ced6659ecf
23209zl <23209zl@hvhs.school.nz>
2026-06-28 23:58:29 +0000

    I made a change

--- fbjs/fb_script.js
+++ fbjs/fb_script.js
@@ -4,6 +4,11 @@ function fb_write() {
     var userName = document.getElementById("name").value;
     const UID = GLOBAL_user["uid"];
 
+     if (userName == "" || userAge == "" || !isNaN(userName) || userAge < 13 || userAge > 99) {
+    alert("Please enter a valid name and age.");
+    return;
+}
+
     firebase.database().ref('/users/' + UID).update({
         name: userName,
         age: userAge,
