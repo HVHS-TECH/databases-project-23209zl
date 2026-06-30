7140e2add346139ab01f3d5423d8eaced6516591
23209zl <23209zl@hvhs.school.nz>
2026-06-30 07:34:37 +0000

    I made a change

---
+++ Leaderboard.js
@@ -0,0 +1,78 @@
+firebase.database().ref("users").once("value", function(snapshot) {
+
+    let basketballScores = [];
+
+    snapshot.forEach(function(childSnapshot) {
+
+        basketballScores.push(childSnapshot.val());
+
+    });
+
+    basketballScores.sort(function(a, b) {
+
+        return b.Basketball - a.Basketball;
+
+    });
+
+    let leaderboard = document.getElementById("basketballList");
+
+    leaderboard.innerHTML = "";
+
+    for (let i = 0; i < 5; i++) {
+
+        if (basketballScores[i]) {
+
+            let player = basketballScores[i];
+
+            leaderboard.innerHTML +=
+                "<li>" +
+                player.displayName +
+                " - " +
+                player.Basketball +
+                "</li>";
+
+        }
+
+    }
+
+});
+
+
+firebase.database().ref("users").once("value", function(snapshot) {
+
+    let geoDashScores = [];
+
+    snapshot.forEach(function(childSnapshot) {
+
+        geoDashScores.push(childSnapshot.val());
+
+    });
+
+    geoDashScores.sort(function(a, b) {
+
+        return (b.GeoDash || 0) - (a.GeoDash || 0);
+
+    });
+
+    let leaderboard = document.getElementById("geoDashList");
+
+    leaderboard.innerHTML = "";
+
+    for (let i = 0; i < 5; i++) {
+
+        if (geoDashScores[i]) {
+
+            let player = geoDashScores[i];
+
+            leaderboard.innerHTML +=
+                "<li>" +
+                player.displayName +
+                " - " +
+                (player.GeoDash || 0) +
+                "</li>";
+
+        }
+
+    }
+
+});
